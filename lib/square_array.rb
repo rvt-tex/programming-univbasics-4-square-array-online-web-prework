2c705811f6ac435211d67fbac3a0715040ee1722
Ruel Tingling <rvtingling@yahoo.com>
2019-08-31 23:02:50 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,17 @@
-def square_array(array)
+#def square_array(array)
   # your code here
+#end
+
+def square_array(array)
+  numbers = [1,2,3]
+  array = numbers
+  counter = 0
+  while numbers[counter] do
+    puts numbers[counter]
+    counter += 1
+  end
 end
+
+numbers = [1,2,3]
+square_array([1,2,3])).to eq([1,4,9])
+puts square_array

be517bd4901b00fc0792197fbe9828c49308549c
Ruel Tingling <rvtingling@yahoo.com>
2019-08-31 22:57:21 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -6,3 +6,12 @@ def square_array(array)
   numbers = [1,2,3]
   array = numbers
   counter = 0
+  while numbers[counter] do
+    puts numbers[counter]
+    counter += 1
+  end
+end
+
+numbers = [1,2,3]
+square_array = [(1**2), (2**2), (3**2)]
+puts square_array
